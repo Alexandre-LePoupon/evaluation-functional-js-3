f8a0ae3ab4d55178682ecf15ca68f2e9057393e3
AlexLP <alexlepoupon@gmail.com>
2018-03-02 10:49:32 +0100

    Exercise 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,6 +1,39 @@
-const countSequence = (sequence, input) => {
-  // Your future job begins here ...
-};
+var _ = require("lodash");
+
+const countSequence = _.curry(sequenceFunct);
+
+function sequenceFunct(sequence,input) {
+    
+  if(sequence && input) {
+
+    var n = 0;
+    var pos = 0;
+    var step = sequence.length;
+    
+    while (true) {
+      pos = input.indexOf(sequence, pos);
+      if (pos >= 0) {
+        ++n;
+        pos += step;
+      } else break;
+    }
+    return n;
+    
+  } else {
+    return 0;
+  }
+}
+
+// function curry2(fn) {
+//   return function(firstArg) {
+//     return function(secondArg) {
+//       return fn(firstArg,secondArg);
+//     }
+//   }
+// }
+
+// var sequenceFunctCurried = curry2(sequenceFunct);
+
 
 module.exports = {
   title: "Exercise 4",
